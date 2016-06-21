8ab78674416c369cf83187a84c501265fea9521e
Curtis Rueden <ctrueden@wisc.edu>
2016-06-21 16:14:37 -0400

    Clean up VisAD transforms and add javadoc

--- src/main/java/net/imagej/visad/GriddedSetTransform.java
+++ src/main/java/net/imagej/visad/GriddedSetTransform.java
@@ -1,3 +1,4 @@
+
 package net.imagej.visad;
 
 import net.imglib2.RealLocalizable;
@@ -7,9 +8,23 @@ import net.imglib2.realtransform.InvertibleRealTransform;
 import visad.GriddedDoubleSet;
 import visad.VisADException;
 
+/**
+ * An ImgLib2 {@link InvertibleRealTransform} backed by a
+ * {@link GriddedDoubleSet}.
+ * <p>
+ * The source space set's continuous M-dimensional grid. The target space is the
+ * N-dimensional value coordinates given by the set.
+ * </p>
+ *
+ * @author Curtis Rueden
+ */
 public class GriddedSetTransform implements InvertibleRealTransform {
 
-	private GriddedDoubleSet set;
+	private final GriddedDoubleSet set;
+
+	public GriddedSetTransform(final GriddedDoubleSet set) {
+		this.set = set;
+	}
 
 	@Override
 	public int numSourceDimensions() {
@@ -22,58 +37,62 @@ public class GriddedSetTransform implements InvertibleRealTransform {
 	}
 
 	@Override
-	public void apply(double[] source, double[] target) {
+	public void apply(final double[] source, final double[] target) {
 		try {
 			unwrap(set.gridToDouble(wrap(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void apply(float[] source, float[] target) {
+	public void apply(final float[] source, final float[] target) {
 		try {
 			unwrap(set.gridToValue(wrap(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void apply(RealLocalizable source, RealPositionable target) {
+	public void apply(final RealLocalizable source,
+		final RealPositionable target)
+	{
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void applyInverse(double[] source, double[] target) {
+	public void applyInverse(final double[] source, final double[] target) {
 		try {
 			unwrap(set.doubleToGrid(wrap(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void applyInverse(float[] source, float[] target) {
+	public void applyInverse(final float[] source, final float[] target) {
 		try {
 			unwrap(set.valueToGrid(wrap(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void applyInverse(RealPositionable source, RealLocalizable target) {
+	public void applyInverse(final RealPositionable source,
+		final RealLocalizable target)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -90,29 +109,29 @@ public class GriddedSetTransform implements InvertibleRealTransform {
 
 	// -- Helper methods --
 
-	private void unwrap(float[][] source, float[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap(final float[][] source, final float[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d][0];
 		}
 	}
 
-	private float[][] wrap(float[] source) {
+	private float[][] wrap(final float[] source) {
 		final float[][] visad = new float[source.length][1];
-		for (int d=0; d<source.length; d++) {
+		for (int d = 0; d < source.length; d++) {
 			visad[d][0] = source[d];
 		}
 		return visad;
 	}
 
-	private void unwrap(double[][] source, double[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap(final double[][] source, final double[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d][0];
 		}
 	}
 
-	private double[][] wrap(double[] source) {
+	private double[][] wrap(final double[] source) {
 		final double[][] visad = new double[source.length][1];
-		for (int d=0; d<source.length; d++) {
+		for (int d = 0; d < source.length; d++) {
 			visad[d][0] = source[d];
 		}
 		return visad;
--- src/main/java/net/imagej/visad/IrregularSetTransform.java
+++ src/main/java/net/imagej/visad/IrregularSetTransform.java
@@ -1,3 +1,4 @@
+
 package net.imagej.visad;
 
 import net.imglib2.RealLocalizable;
@@ -7,9 +8,22 @@ import net.imglib2.realtransform.InvertibleRealTransform;
 import visad.IrregularSet;
 import visad.VisADException;
 
+/**
+ * An ImgLib2 {@link InvertibleRealTransform} backed by an {@link IrregularSet}.
+ * <p>
+ * The source space is the set's discrete 1D indices. The target space is the
+ * N-dimensional value coordinates specified by the set.
+ * </p>
+ *
+ * @author Curtis Rueden
+ */
 public class IrregularSetTransform implements InvertibleRealTransform {
 
-	private IrregularSet set;
+	private final IrregularSet set;
+
+	public IrregularSetTransform(final IrregularSet set) {
+		this.set = set;
+	}
 
 	@Override
 	public int numSourceDimensions() {
@@ -22,58 +36,62 @@ public class IrregularSetTransform implements InvertibleRealTransform {
 	}
 
 	@Override
-	public void apply(double[] source, double[] target) {
+	public void apply(final double[] source, final double[] target) {
 		try {
 			unwrap_f2_d1(set.indexToValue(wrap_d1_i1(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void apply(float[] source, float[] target) {
+	public void apply(final float[] source, final float[] target) {
 		try {
 			unwrap_f2_f1(set.indexToValue(wrap_f1_i1(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void apply(RealLocalizable source, RealPositionable target) {
+	public void apply(final RealLocalizable source,
+		final RealPositionable target)
+	{
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void applyInverse(double[] source, double[] target) {
+	public void applyInverse(final double[] source, final double[] target) {
 		try {
 			unwrap_i1_d1(set.valueToIndex(wrap_d1_f2(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void applyInverse(float[] source, float[] target) {
+	public void applyInverse(final float[] source, final float[] target) {
 		try {
 			unwrap_i1_f1(set.valueToIndex(wrap_f1_f2(source)), target);
 		}
-		catch (VisADException exc) {
+		catch (final VisADException exc) {
 			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 	}
 
 	@Override
-	public void applyInverse(RealPositionable source, RealLocalizable target) {
+	public void applyInverse(final RealPositionable source,
+		final RealLocalizable target)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -90,67 +108,67 @@ public class IrregularSetTransform implements InvertibleRealTransform {
 
 	// -- Helper methods --
 
-	private int[] wrap_f1_i1(float[] source) {
+	private int[] wrap_f1_i1(final float[] source) {
 		final int[] visad = new int[source.length];
-		for (int d=0; d<source.length; d++) {
+		for (int d = 0; d < source.length; d++) {
 			visad[d] = clip(round(source[d])); // nearest neighbor!
 		}
 		return visad;
 	}
 
-	private int[] wrap_d1_i1(double[] source) {
+	private int[] wrap_d1_i1(final double[] source) {
 		final int[] visad = new int[source.length];
-		for (int d=0; d<source.length; d++) {
+		for (int d = 0; d < source.length; d++) {
 			visad[d] = clip(round(source[d])); // nearest neighbor!
 		}
 		return visad;
 	}
 
-	private float[][] wrap_f1_f2(float[] source) {
-		float[][] visad = new float[source.length][1];
-		for (int d=0; d<source.length; d++) {
+	private float[][] wrap_f1_f2(final float[] source) {
+		final float[][] visad = new float[source.length][1];
+		for (int d = 0; d < source.length; d++) {
 			visad[d][0] = source[d];
 		}
 		return visad;
 	}
 
-	private float[][] wrap_d1_f2(double[] source) {
-		float[][] visad = new float[source.length][1];
-		for (int d=0; d<source.length; d++) {
+	private float[][] wrap_d1_f2(final double[] source) {
+		final float[][] visad = new float[source.length][1];
+		for (int d = 0; d < source.length; d++) {
 			visad[d][0] = (float) source[d];
 		}
 		return visad;
 	}
 
-	private void unwrap_f2_f1(float[][] source, float[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap_f2_f1(final float[][] source, final float[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d][0];
 		}
 	}
 
-	private void unwrap_f2_d1(float[][] source, double[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap_f2_d1(final float[][] source, final double[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d][0];
 		}
 	}
 
-	private void unwrap_i1_f1(int[] source, float[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap_i1_f1(final int[] source, final float[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d];
 		}
 	}
 
-	private void unwrap_i1_d1(int[] source, double[] target) {
-		for (int d=0; d<source.length; d++) {
+	private void unwrap_i1_d1(final int[] source, final double[] target) {
+		for (int d = 0; d < source.length; d++) {
 			target[d] = source[d];
 		}
 	}
 
-	private int clip(long round) {
+	private int clip(final long round) {
 		return (round > Integer.MAX_VALUE) ? Integer.MAX_VALUE : (int) round;
 	}
 
-	private long round(double d) {
+	private long round(final double d) {
 		return Math.round(d);
 	}
 
