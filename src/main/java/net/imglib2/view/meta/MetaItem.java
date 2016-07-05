dbc6a01b0a56b53d27dcd0b4e012d390a8c79621
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-07-05 08:07:02 +0200

    remove unused classes

--- src/main/java/net/imglib2/view/meta/MetaItem.java
+++
@@ -1,9 +0,0 @@
-package net.imglib2.view.meta;
-
-import net.imglib2.RandomAccessible;
-
-// TODO: rename MetaDatum
-public interface MetaItem< T >
-{
-	public RandomAccessible< T > randomAccessible();
-}
